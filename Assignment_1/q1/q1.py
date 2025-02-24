3ef593c217933e59a2d6e3d0c2984484c4ca8779
Ankit Singh <105532883+zakmii@users.noreply.github.com>
2025-02-24 16:21:02 +0530

    Add functionality to read prime numbers from files and update Bob's key generation process (q1 done)

--- Assignment_1/q1/q1.py
+++ Assignment_1/q1/q1.py
@@ -3,6 +3,11 @@ from os import urandom
 from gmpy2 import invert, powmod
 from  typing import Optional, Tuple
 
+def read_prime_from_file(filename: str) -> int:
+    with open(filename, 'r') as file:
+        prime_str = file.read().strip()
+    return int(prime_str)
+
 class Alice:
     def __init__(self):
         self.__symmetric_key: Optional[bytes] = None
@@ -72,7 +77,7 @@ class Bob:
         if message is None:
             print("Message is not provided so generating a random message")
             message = urandom(16)
-            print("Generated message: ", message)
+            print("Generated message: ", message, "\n")
         
         cipher = Salsa20.new(key=self.__alice_symmetric_key)
         cipher_text = cipher.nonce + cipher.encrypt(message)
@@ -88,3 +93,39 @@ alice.generate_symmetric_key()
 print("Alice's symmetric key: ", alice.get_symmetric_key())
 
 #Step 2: Bob generates a public and private key using RSA
+prime_p = input("Enter the prime number p: ")
+
+if prime_p.strip() == "":
+    print("Reading prime number p from file", "\n")
+    prime_p = read_prime_from_file("Assignment_1\q1\prime_p.txt")
+
+prime_q = input("Enter the prime number q: ")
+
+if prime_q.strip() == "":
+    print("Reading prime number q from file", "\n")
+    prime_q = read_prime_from_file("Assignment_1\q1\prime_q.txt")
+
+bob.generate_key_using_rsa(prime_p, prime_q)
+print("Bob's public key: (n , e)", bob.get_public_key(), "\n")
+print("Bob's private key: (n , d)", bob.get_private_key(), "\n")
+
+#Step 3: Alice encrypts the symmetric key using Bob's public key
+alice_symmetric_key = alice.get_symmetric_key()
+print("Alice's symmetric key: ", alice_symmetric_key, "\n")
+
+cipher_text =alice.encrypt_message(alice_symmetric_key, bob.get_public_key())
+print("c: Cipher text from Alice (encrypted symmetric key)-: ", cipher_text, "\n")
+
+#Step 4: Bob decrypts the symmetric key using his private key
+print("Symmetric key decrypted by Bob: ", bob.decrypt_alice_symmetric_key(cipher_text), "\n")
+
+#Step 5: Bob encrypts a message using the symmetric key
+message = input("Enter the message to be encrypted: ")
+if message.strip() == "":
+    message = None
+
+cipher_text = bob.encrypt_using_symmetric_key()
+print("Cipher text from Bob (encrypted message): ", cipher_text, "\n")
+
+#Step 6: Alice decrypts the message using the symmetric key
+print("Decrypted message Alice: ", alice.decrypt_salsa20_cipher(cipher_text), "\n")
